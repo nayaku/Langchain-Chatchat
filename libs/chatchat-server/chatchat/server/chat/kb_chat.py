a76aa39b6df41fbebffecbb1e1afe6e4aa00d066
nayaku <zmk.code@qq.com>
2025-03-16 15:00:29 +0800

    回退：莫名其妙，就算是空查询结果，也不应该更换回答模板

--- libs/chatchat-server/chatchat/server/chat/kb_chat.py
+++ libs/chatchat-server/chatchat/server/chat/kb_chat.py
@@ -177,8 +177,8 @@ async def kb_chat(query: str = Body(..., description="用户输入", examples=["
             #     print(docs)
             context = "\n\n".join([doc["page_content"] for doc in docs])
 
-            # if len(docs) == 0:  # 如果没有找到相关文档，使用empty模板
-            #     prompt_name = "empty"
+            if len(docs) == 0:  # 如果没有找到相关文档，使用empty模板
+                prompt_name = "empty"
             prompt_template = get_prompt_template("rag", prompt_name)
             input_msg = History(role="user", content=prompt_template).to_msg_template(False)
             chat_prompt = ChatPromptTemplate.from_messages(
